436ff7a6d57fe16f3f61894302a49ae90b9c7009
Alan Tennent <tennent4@gmail.com>
2014-08-12 18:53:32 -0700

    Started FloodIt

---
+++ FloodIt/Assets/Scripts/Board.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+
+	public tilePrefab : Tile;
+
+	// Use this for initialization
+	void Start () {
+		createBoard();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ FloodIt/Assets/Scripts/Tile.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	Transform plane;
+	Transform reversePlane;
+
+	private bool facingForward = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		processFlip();
+
+		if (Input.GetMouseButtonDown(0)) {
+			flip();
+		}
+	}
+
+	void flip() {
+		facingForward = !facingForward;
+	}
+
+	void processFlip() {
+		float desiredY = (facingForward) ? 0f : 180f;
+
+		Vector3 currentRotation = transform.rotation.eulerAngles;
+		Vector3 transitionRotation = Vector3.Lerp(currentRotation, new Vector3(0, desiredY, 0), 0.25f);
+
+		transform.rotation = Quaternion.Euler(transitionRotation);
+	}
+}
